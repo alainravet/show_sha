46b9ded36a5ed68265333579cd42aa6b1e38f7e9
Alain Ravet <alainravet@gmail.com>
2011-05-25 19:20:53 +0200

    enginex show_sha

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c8e03baa275ec5a884b8feba0bc64c699e0aa2d3bcd9bedcc0a0c75b439009d79def52dc53a7c4cea5f9ee0d59bcf336d93c75b2aca46b19f60211e65edb31b6'
